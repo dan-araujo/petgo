6ae6c6a5a4c308bfb2e2b80c4fb22f398423b9ef
Daniel Araújo <daniel.ti.info@gmail.com>
2025-10-23 09:55:39 -0300

    Update auth_service.dart

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,9 +1,11 @@
-import 'package:petgo/models/user_model.dart';
+import 'package:petgo/models/customer_model.dart';
 
 Future<void> sendVerificationCode(String emailOrPhone) async {
   // futuramente vai enviar o código OTP (por e-mail ou SMS)
 }
 
-Future<UserModel?> verifyCode(String emailOrPhone, String code) async {
+Future<CustomerModel?> verifyCode(String emailOrPhone, String code) async {
+  return null;
+
   // futuramente valida o código e retorna o usuário autenticado
 }
