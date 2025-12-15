ac831635b766796968a9ec153d7d2815fb2cd720
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-15 14:58:10 -0300

    feat(login): create the login page for store

--- lib/features/auth/screens/login/store_login_screen.dart
+++ lib/features/auth/screens/login/store_login_screen.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:petgo/features/auth/screens/register/register_store_screen.dart';
+import 'package:petgo/features/auth/screens/register/store_register_screen.dart';
+import 'package:petgo/features/auth/services/auth_service.dart';
+import 'package:petgo/features/auth/services/token_service.dart';
 import 'package:petgo/features/auth/widgets/login/login_base_screen.dart';
+import 'package:petgo/features/store/screens/store_home_screen.dart';
 
 class StoreLoginScreen extends StatelessWidget {
   const StoreLoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authService = AuthService();
+
     return LoginBaseScreen(
       title: 'Login do Parceiro',
       subtitle: 'Acesse sua conta para gerenciar pedidos e produtos.',
       imagePath: 'assets/images/store_login_cat.png',
       backgroundColor: const Color(0xFFFFF7F4),
-      buttonColor: const Color(0xFFFF8C8C),
+      buttonColor: const Color(0xFFEC5050),
       onRegisterTap: () {
-        Navigator.push(context, 
-        MaterialPageRoute(builder: (context) => const RegisterStoreScreen()),
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const StoreRegisterScreen()),
         );
       },
       onLogin: (email, password) async {
-        await Future.delayed(const Duration(seconds: 1));
+        final result = await authService.loginStore(email, password);
+
+        if(!context.mounted) return;
+
+        await TokenService.saveToken(result.accessToken);
+        await TokenService.saveUser(
+          result.user.id,
+          result.user.name,
+          result.user.email,
+        );
+
+        if(!context.mounted) return;
+        
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                CustomerHomeScreen(userName: result.user.name),
+          ),
+        );
       },
     );
   }
