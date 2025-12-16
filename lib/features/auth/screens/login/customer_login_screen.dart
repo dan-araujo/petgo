86504181dfafa39a02fb00eef0aa57d4fa40f559
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-16 14:48:16 -0300

    refactor(login): change the image and colors for customer login page

--- lib/features/auth/screens/login/customer_login_screen.dart
+++ lib/features/auth/screens/login/customer_login_screen.dart
@@ -3,6 +3,7 @@ import 'package:petgo/features/auth/screens/register/customer_register_screen.da
 import 'package:petgo/features/auth/services/auth_service.dart';
 import 'package:petgo/features/auth/services/token_service.dart';
 import 'package:petgo/features/auth/widgets/login/login_base_screen.dart';
+import 'package:petgo/features/customer/screens/customer_home_screen.dart';
 
 class CustomerLoginScreen extends StatelessWidget {
   const CustomerLoginScreen({super.key});
@@ -14,9 +15,9 @@ class CustomerLoginScreen extends StatelessWidget {
     return LoginBaseScreen(
       title: 'Login do Cliente',
       subtitle: 'Acesse sua conta para oferecer o melhor para seu companheiro.',
-      imagePath: 'assets/images/customer_login_friends.png',
-      backgroundColor: const Color(0xFFFFF3E0),
-      buttonColor: const Color(0xFFF28C2B),
+      imagePath: 'assets/images/login_customer.png',
+      backgroundColor: const Color(0xFFE8E1FD),
+      buttonColor: const Color(0xFF2596BE),
       onRegisterTap: () {
         Navigator.push(
           context,
@@ -32,6 +33,16 @@ class CustomerLoginScreen extends StatelessWidget {
           result.user.id,
           result.user.name,
           result.user.email,
+        );
+
+        if (!context.mounted) return;
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                CustomerHomeScreen(userName: result.user.name),
+          ),
         );
       },
     );
