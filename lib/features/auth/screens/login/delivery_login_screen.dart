45984cd3c7d5de288e0c23ccd096642abc115ac3
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-16 14:48:36 -0300

    feat(login): implements the delivery login page

---
+++ lib/features/auth/screens/login/delivery_login_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:petgo/features/auth/screens/register/delivery_register_screen.dart';
+import 'package:petgo/features/auth/services/auth_service.dart';
+import 'package:petgo/features/auth/services/token_service.dart';
+import 'package:petgo/features/auth/widgets/login/login_base_screen.dart';
+import 'package:petgo/features/delivery/screens/delivery_home_screen.dart';
+
+class DeliveryLoginScreen extends StatelessWidget {
+  const DeliveryLoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = AuthService();
+
+    return LoginBaseScreen(
+      title: 'Login do Entregador',
+      subtitle: 'Sua conta para entregar alegria aos tutores de pets.',
+      imagePath: 'assets/images/login_delivery.png',
+      imageHeight: 190,
+      backgroundColor: const Color(0xFFE5F8E5),
+      buttonColor: const Color(0xFF85AB6D),
+      onRegisterTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const DeliveryRegisterScreen()),
+        );
+      },
+      onLogin: (email, password) async {
+        final result = await authService.loginDelivery(email, password);
+
+        if(!context.mounted) return;
+
+        await TokenService.saveToken(result.accessToken);
+        await TokenService.saveUser(
+          result.user.id,
+          result.user.name,
+          result.user.email,
+        );
+
+        if(!context.mounted) return;
+        
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                DeliveryHomeScreen(userName: result.user.name),
+          ),
+        );
+      },
+    );
+  }
+}
