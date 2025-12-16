2fc0f31762e82681d33991a4d73e84bcaa82a700
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-16 14:36:07 -0300

    feat(constants): create global constants for the api

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  static const String baseUrl = "https://petgo-backend.onrender.com";
+  static const String appName = 'PetGo!';
+  static const Map<String, String> defaultHeaders = {
+    'Content-Type': 'application/json',
+  };
+
+  static const Duration requestTimeOut = Duration(seconds: 30);
+  static const Duration connectionTimeout = Duration(seconds: 30);
+
+  static const String loginEndpoint = '/auth/login';
+  static const String sendCodeEndpoint = '/auth/send-verification-code';
+  static const String verifyCodeEndpoint = '/auth/verify-code';
+}
