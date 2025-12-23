97079536994bb92b136d26f6ebebb5b91d064369
Daniel Araújo <77083737+dan-araujo@users.noreply.github.com>
2025-12-23 16:17:39 -0300

    refactor(routes): add auth routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petgo/routes/auth_routes.dart';
 import 'package:petgo/routes/customer_routes.dart';
 import 'package:petgo/routes/delivery_routes.dart';
 import 'package:petgo/routes/store_routes.dart';
@@ -7,6 +8,7 @@ import 'package:petgo/routes/veterinary_routes.dart';
 class AppRoutes {
   static Map<String, WidgetBuilder> getRoutes() {
     return {
+      ...AuthRoutes.getRoutes(),
       ...VeterinaryRoutes.getRoutes(),
       ...CustomerRoutes.getRoutes(),
       ...StoreRoutes.getRoutes(),
